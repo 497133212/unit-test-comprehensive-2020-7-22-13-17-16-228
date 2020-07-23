95bd0611d932742ae2bc11d868ae982fb097d1d7
Austin <497133212@qq.com>
2020-07-23 21:59:36 +0800

    [GuessNumber][Austin] feature: implement generate method

--- src/main/java/example/AnswerGenerateImplement.java
+++ src/main/java/example/AnswerGenerateImplement.java
@@ -5,6 +5,17 @@ import java.util.Random;
 public class AnswerGenerateImplement implements AnswerGenerate{
     @Override
     public String generate() {
-        return "1234";
+        StringBuffer stringBuffer = new StringBuffer();
+        Random ran = new Random();
+        int i = 0;
+        while(i < 4){
+            int number = ran.nextInt(9);
+            if(stringBuffer.indexOf(String.valueOf(number)) == -1){
+                stringBuffer.append(number);
+                i++;
+            }
+        }
+        String answer = stringBuffer.toString();
+        return answer;
     }
 }
