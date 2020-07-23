dc48b012cceed9e1ff71214870db751af3f39b17
Austin <497133212@qq.com>
2020-07-23 21:47:50 +0800

    [GuessNumber][Austin] feature: implement number partly right and position partly right

--- src/main/java/example/GameNumberGame.java
+++ src/main/java/example/GameNumberGame.java
@@ -7,10 +7,27 @@ public class GameNumberGame {
         this.answer = answer;
     }
 
+    public GameNumberGame(AnswerGenerate answerGenerate) {
+        this.answer = answerGenerate.generate();
+    }
+
     public String guess(String guess) {
-        if(answer.equals(guess)) {
-            return "4A0B";
+        int allRight = 0;
+        int positionRight = 0;
+        String number = answer;
+        for(int i=0;i<guess.length();i++) {
+            String guessSubstring = guess.substring(i,i+1);
+            boolean flag = number.contains(guessSubstring);
+            if(flag) {
+                int index = number.indexOf(guessSubstring);
+                if(index == i) {
+                    allRight++;
+                } else {
+                    positionRight++;
+                }
+            }
         }
-        return "2A2B";
+        String result = String.format("%sA%sB",allRight, positionRight);
+        return result;
     }
 }
