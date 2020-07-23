67b7a7b0b8d7d9f830bb4d0c098a684bf3accd0c
Austin <497133212@qq.com>
2020-07-23 21:58:07 +0800

    [GuessNumber][Austin] feature: add generate method

---
+++ src/main/java/example/AnswerGenerateImplement.java
@@ -0,0 +1,10 @@
+package example;
+
+import java.util.Random;
+
+public class AnswerGenerateImplement implements AnswerGenerate{
+    @Override
+    public String generate() {
+        return "1234";
+    }
+}
