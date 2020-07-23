8458a589d094525028ab4883a1f86b7a86d0b4b6
Austin <497133212@qq.com>
2020-07-23 16:59:01 +0800

    [][Austin] feature: implement number all right and position all right

---
+++ src/main/java/example/GameNumberGame.java
@@ -0,0 +1,13 @@
+package example;
+
+public class GameNumberGame {
+    private String answer;
+
+    public GameNumberGame(String answer) {
+        this.answer = answer;
+    }
+
+    public String guess(String guess) {
+        return "4A0B";
+    }
+}
