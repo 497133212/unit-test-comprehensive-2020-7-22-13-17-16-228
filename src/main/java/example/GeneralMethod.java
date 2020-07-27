4bdf14bcde14536bdf588624ee6a0a1aa1238172
Austin <497133212@qq.com>
2020-07-27 19:36:28 +0800

    [GuessNumber][Austin] refactor: split inputGuessNumber and judgeGuessResult

--- src/main/java/example/GeneralMethod.java
+++ src/main/java/example/GeneralMethod.java
@@ -1,8 +1,37 @@
 package example;
 
+import java.util.Scanner;
+
 public class GeneralMethod {
 
     public void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public String inputGuessNumber() {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder guess = new StringBuilder();
+        for (int j = 0; j < 4; j++) {
+            guess.append(scanner.nextInt());
+        }
+        return guess.toString();
+    }
+
+    public void JudgeGuessResult(String answer) {
+        for (int i = 1; i <= 6; i++) {
+            String guess = inputGuessNumber();
+            VerificationInputNumber verificationInputNumber = new VerificationInputNumber();
+            if (verificationInputNumber.verificationGuessNumber(guess)) {
+                GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+                String result = guessNumberGame.guess(guess);
+                System.out.println(result);
+                if ("4A0B".equals(result)) {
+                    this.printMessage("you win!");
+                    break;
+                }
+            } else {
+                this.printMessage("Wrong Input，Input again");
+            }
+        }
     }
 }
--- src/main/java/example/MainProcess.java
+++ src/main/java/example/MainProcess.java
@@ -3,33 +3,16 @@ package example;
 import java.util.Scanner;
 
 public class MainProcess {
+
+    public static final String INPUT_GUESS_NUMBER = "Please enter the number you want to guess. You have a total of six chances.";
+
     public static void main(String[] args) {
         AnswerGenerate answerGenerate = new AnswerGenerateImplement();
         GeneralMethod generalMethod = new GeneralMethod();
         String answer = answerGenerate.generate();
         generalMethod.printMessage("Answer:" + answer);
-        generalMethod.printMessage("Please enter the number you want to guess. You have a total of six chances.");
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 1; i <= 6; i++) {
-            StringBuilder guess = new StringBuilder();
-            for (int j = 0; j < 4; j++) {
-                guess.append(scanner.nextInt());
-            }
-            VerificationInputNumber verificationInputNumber = new VerificationInputNumber();
-            boolean flag = verificationInputNumber.verificationGuessNumber(guess.toString());
-            if (flag) {
-                GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
-                String result = guessNumberGame.guess(guess.toString());
-                System.out.println(result);
-                if ("4A0B".equals(result)) {
-                    generalMethod.printMessage("you win!");
-                    break;
-                }
-            } else {
-                generalMethod.printMessage("Wrong Input，Input again");
-            }
-        }
-
+        generalMethod.printMessage(INPUT_GUESS_NUMBER);
+        generalMethod.JudgeGuessResult(answer);
     }
 
 }
